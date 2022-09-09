ccd9c3d76aae7bc00188ed06fc5c0938db42e3dc
mhadziq0403 <mhadziq0403@gmail.com>
2022-09-09 20:12:44 +0700

    Menyelesaikan views, urls, models, html

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from katalog.models import CatalogItem
+
 # TODO: Create your views here.
+def show_catalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_catalog': data_catalog_item,
+        'nama': 'Muhammad Hadziq Razin',
+        'id': '2106707076',
+    }
+    return render(request, "katalog.html", context)
