15aaf72a788c0624fbcca8eacfa0ad2cc30b4a9c
mhadziq0403 <mhadziq0403@gmail.com>
2022-09-20 16:11:57 +0700

    Melanjutkan Tugas 3

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class MyWatchlist(models.Model):
     watched = models.CharField(max_length=20)
     title = models.CharField(max_length=150)
-    rating = models.IntegerField()
+    rating = models.CharField(max_length=1)
     release_date = models.CharField(max_length=40)
     review = models.TextField()
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -5,9 +5,19 @@ from mywatchlist.models import MyWatchlist
 # TODO: Create your views here.
 def show_watchlist(request):
     data_watchlist_item = MyWatchlist.objects.all()
+    count = 0
+    for movie in data_watchlist_item:
+        if movie.watched == "Already":
+            count += 1
+    if count >= 5:
+        pesan = "Selamat, kamu sudah banyak menonton!"
+    else:
+        pesan = "Wah, kamu masih sedikit menonton!"
+
     context = {
         'data_watchlist': data_watchlist_item,
         'nama': 'Muhammad Hadziq Razin',
         'id': '2106707076',
+        'pesan': pesan
     }
     return render(request, "mywatchlist.html", context)
