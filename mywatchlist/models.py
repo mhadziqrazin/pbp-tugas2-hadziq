fb1ab08aafcdd7e953fe5db33b8d0e1be5597ff7
mhadziq0403 <mhadziq0403@gmail.com>
2022-09-21 23:33:08 +0700

    Membenarkan max_length untuk rating

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class MyWatchlist(models.Model):
     watched = models.CharField(max_length=20)
     title = models.CharField(max_length=150)
-    rating = models.CharField(max_length=1)
+    rating = models.CharField(max_length=3)
     release_date = models.CharField(max_length=40)
     review = models.TextField()
