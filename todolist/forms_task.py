aae7b21b689b75cf96908e55803940e4248cc2c8
mhadziqrazin <88391977+mhadziqrazin@users.noreply.github.com>
2022-09-27 23:49:07 +0700

    Delete forms_task.py

--- todolist/forms_task.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from todolist.models import Task
-
-class CreateNewTask(ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'description']
