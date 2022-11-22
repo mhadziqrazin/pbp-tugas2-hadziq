58244122d531d7348ff9e7e7efc3a9d1d24a3a8d
mhadziq0403 <mhadziq0403@gmail.com>
2022-11-22 23:57:51 +0700

    Menambahkan Allow Headers

--- project_django/settings.py
+++ project_django/settings.py
@@ -60,7 +60,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'project_django.urls'
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_ALL_HEADERS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 TEMPLATES = [
     {
